efdf42f6003cb5af7eba1885badd4370954b1cb9
krupa.nanda <krupananda91@gmail.com>
2023-07-21 14:49:54 +0530

    cool

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance"  "krupa-web" {
-  ami           = "ami-05548f9cecf47b442"
+   ami           = "ami-05548f9cecf47b442"
   instance_type = "t2.micro"
 key_name = "aaaa"
   tags = {
