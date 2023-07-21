a43c15d2f1f8fa58e4d77d86eb1d63a3b34b5e62
krupa.nanda <krupananda91@gmail.com>
2023-07-21 13:58:52 +0530

    added terraform script to create ec2 instance of amazon linux2 ami

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance"  "krupa-web" {
+  ami           = "ami-05548f9cecf47b442"
+  instance_type = "t2.micro"
+key_name = "aaaa"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
---
+++ providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+   access_key = "AKIAYYO2XZRESQFWTVEJ"
+  secret_key = "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"
+}
+
+
